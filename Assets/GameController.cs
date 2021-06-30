c180a13d8ba2e282fb8b011bc73e0d016531d3aa
CoffeePasta <81752754+CoffeePasta@users.noreply.github.com>
2021-06-30 15:41:02 +0700

    Init commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private TrailController trailController;
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+
+    private bool _isGameEnded = false;
+
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed -= ChangeBird;
+            Birds[i].OnBirdShot -= AssignTrail;
+        }
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded || Birds.Count <= 0) return;
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TapCollider.enabled = true;
+        trailController.SetBird(bird);
+        StartCoroutine(trailController.SpawnTrail());
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+}
---
+++ Assets/OwlBird.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    private float _originRad;
+    public float _explotionRadius = 10f;
+    public bool _isExploded = false;
+
+    public override void OnTap()
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        if (State != BirdState.Idle && !_isExploded)
+        {
+            _originRad = collider.radius;
+            rigidBody.mass *= 5;
+            collider.radius *= _explotionRadius;
+            _isExploded = true;
+
+            Invoke("Exploded", 0.05f);
+        }
+    }
+
+    public void Exploded()
+    {
+        collider.radius = _originRad;
+    }
+}
